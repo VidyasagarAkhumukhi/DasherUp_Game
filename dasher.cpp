704fdffd39dc48340cb297c347461df1388be380
Vidyasagar Akhumukhi <sagar112113@gmail.com.com>
2025-08-08 16:02:15 +0100

    first commit

---
+++ dasher.cpp
@@ -0,0 +1,66 @@
+#include "raylib.h";
+
+int main()
+{
+    const int windowWidth{1024};
+    int windowHeight{720};
+
+    InitWindow(windowWidth, windowHeight, "Dasher");
+
+    // acceleration due to gravity (pixels/frame)/frame
+    const int gravity{1};
+
+    // special compound variable Texture2D by raylib to save textures in variables.
+    Texture2D scarfy = LoadTexture("textures/scarfy.png");
+    Rectangle scarfyRec;
+    scarfyRec.width = (scarfy.width / 6);
+    scarfyRec.height = scarfy.height;
+    scarfyRec.x = 0;
+    scarfyRec.y = 0;
+
+    Vector2 scarfyPos;
+    scarfyPos.x = (windowWidth / 3) - (scarfyRec.width / 2);
+    scarfyPos.y = (windowHeight - scarfyRec.height);
+
+    const int jumpVel{-25};
+
+    // int posY{windowHeight - scarfyRec.height};
+    int velocity{0};
+
+    SetTargetFPS(180);
+
+    while (!WindowShouldClose())
+    {
+        // start Drawing
+        BeginDrawing();
+        ClearBackground(SKYBLUE);
+
+        // performing ground check
+        if (scarfyPos.y >= (windowHeight - scarfyRec.height))
+        {
+            // scarfy is on the ground
+            velocity = 0;
+        }
+        else
+        {
+            // scarfy is in the air
+            //  apply gravity
+            velocity += gravity;
+        }
+
+        if (IsKeyPressed(KEY_SPACE) && (scarfyPos.y >= (0 + scarfyRec.height))) // stops scarfy from escaping the upper bounds
+        {
+            velocity += jumpVel;
+        };
+
+        // updating position
+        scarfyPos.y += velocity;
+
+        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
+
+        // stop Drawing
+        EndDrawing();
+    }
+    UnloadTexture(scarfy);
+    CloseWindow();
+};
